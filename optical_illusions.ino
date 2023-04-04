8e0de996e037ee90f13ee1b306989d95bd7e1ba1
charles <cgiguere@iname.com>
2023-04-04 18:14:27 -0400

    drawHermannGrid

--- optical_illusions.ino
+++ optical_illusions.ino
@@ -10,13 +10,47 @@
 #define LCD_RD A0    // LCD Read goes to Analog 0
 #define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
 
+// Color definitions
+#define COLOR_BLACK 0x0000       ///<   0,   0,   0
+#define COLOR_WHITE 0xFFFF     ///< 255, 255, 255
+
 Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+
+const int SCREEN_WIDTH  = tft.width();
+const int SCREEN_HEIGHT = tft.height();
 
 void setup(void) {
   Serial.begin(9600);
+  tft.reset();
+  uint16_t identifier = tft.readID();
+  tft.begin(identifier);
 
+  drawHermannGrid();
 }
 
 void loop(void) {
 
+}
+
+void drawHermannGrid() {
+  int gridSize = 10;
+  int squareSize = 10;
+  int lineWidth = 2;
+
+  clearScreen();
+
+  // Draw vertical lines
+  for (int i = 0; i <= gridSize; i++) {
+    tft.fillRect(i * (squareSize + lineWidth), 0, lineWidth, SCREEN_HEIGHT, COLOR_WHITE);
+  }
+
+  // Draw horizontal lines
+  for (int i = 0; i <= gridSize; i++) {
+    tft.fillRect(0, i * (squareSize + lineWidth), SCREEN_WIDTH, lineWidth, COLOR_WHITE);
+  }
+}
+
+void clearScreen(void)
+{
+  tft.fillScreen(COLOR_BLACK);
 }
