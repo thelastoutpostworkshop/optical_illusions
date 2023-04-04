effd902be964231c8eaa5335de917ef011df9d53
charles <cgiguere@iname.com>
2023-04-04 18:08:47 -0400

    Adafruit_GFX

--- optical_illusions.ino
+++ optical_illusions.ino
@@ -1,7 +1,22 @@
+#include <Adafruit_TFTLCD.h> // Hardware-specific library
+#include <Adafruit_GFX.h>    // Core graphics library
+
+// The control pins for the LCD can be assigned to any digital or
+// analog pins...but we'll use the analog pins as this allows us to
+// double up the pins with the touch screen (see the TFT paint example).
+#define LCD_CS A3    // Chip Select goes to Analog 3
+#define LCD_CD A2    // Command/Data goes to Analog 2
+#define LCD_WR A1    // LCD Write goes to Analog 1
+#define LCD_RD A0    // LCD Read goes to Analog 0
+#define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
+
+Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+
 void setup(void) {
+  Serial.begin(9600);
 
 }
 
 void loop(void) {
-    
+
 }
